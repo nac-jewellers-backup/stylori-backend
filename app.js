1c21f5e4a2c4c05a0c55a36a305a3292164a6219
sandeep <sandeep0814@gmail.com>
2019-07-08 16:17:38 +0530

    adding buildspec

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-    res.status(200).send({ message: "Server is up & running on " });
+    res.status(200).send({ message: "Server is up & running" });
   });
 
 app.listen(8000);
