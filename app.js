1511b1e87abc5060a4af79e31905575241b6a9c5
sandeep <sandeep0814@gmail.com>
2019-07-08 15:38:59 +0530

    adding test route

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.status(200).send({ message: "Server is up & running on " });
+  });
+
 app.listen(8000);
