b644e10748a2623d69b6bf38d63257dafe56f6dd
sandeep <sandeep0814@gmail.com>
2019-07-08 16:41:32 +0530

    building image

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-    res.status(200).send({ message: "Server is up & running" });
+    res.status(200).send({ message: "Server is up & running on 80" });
   });
 
 app.listen(8000);
