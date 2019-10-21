bcc1306cace5a14fc85fbe6347fb62019091d4ea
manokarantk <manotk.developer@gmail.com>
2019-10-21 15:13:04 +0530

    update filter dependency

--- controller/filtercontroller.js
+++ controller/filtercontroller.js
@@ -8,19 +8,67 @@ var splitArray = require('split-array');
 
 exports.filteroptions = async (req, res) => {
 
-const {product_type } = req.body
+const {material, theme, occassion, style } = req.body
 var product_list = [];
-//var whereclause = {};
+var whereclause = {};
+var includeclause = [];
 
-// var products = await models.product_lists.findAll({
-//     attributes:['product_id'],
-//     where: {
-//        // product_type
-//     }
-//   })
-// products.forEach(element => {
-//     product_list.push(element.product_id);
-// });
+// [
+//   {
+//    model : models.product_materials
+//   },
+//   {
+//     model : models.product_themes
+//    },{
+//     model : models.product_occassions
+//    },
+//    {
+//     model : models.product_styles
+//    }]
+
+
+// '$product_materials.material_name$':
+//       {
+//       [Op.eq]:material
+//       },
+//       '$product_themes.theme_name$':
+//       {
+//       [Op.eq]:theme
+//       },
+//       '$product_occassions.occassion_name$':
+//           {
+//           [Op.eq]:occassion
+//         },
+//         '$product_styles.style_name$':
+//           {
+//           [Op.eq]:style
+//           },
+if(material)
+{
+  whereclause['$product_materials.material_name$'] = {
+    [Op.eq]:material
+    }
+    includeclause.push({
+      model : models.product_materials
+     })
+}
+if(theme)
+{
+  whereclause['$product_themes.theme_name$'] = {
+    [Op.eq]:theme
+    }
+    includeclause.push({
+           model : models.product_themes
+    })
+}
+var products = await models.product_lists.findAll({
+    attributes:['product_id'],
+    include:includeclause,
+    where: whereclause
+  })
+products.forEach(element => {
+    product_list.push(element.product_id);
+});
 
 
 
@@ -41,12 +89,12 @@ var master_category =    await models.master_product_categories.findAll({
 
   var master_styles = await models.product_styles.findAll({
     attributes: ['style_name'],
-    group: ['style_name']
-    // where: {
-    //   product_id : {
-    //     [Op.in]: product_list
-    //   }
-    // }
+    group: ['style_name'],
+    where: {
+      product_id : {
+        [Op.in]: product_list
+      }
+    }
     
   })
 
@@ -62,32 +110,32 @@ var master_category =    await models.master_product_categories.findAll({
 
   var master_occassion = await models.product_occassions.findAll({
     attributes: ['occassion_name'],
-    group: ['occassion_name']
-    // where: {
-    //   product_id : {
-    //     [Op.in]: product_list
-    //   }
-    // }
+    group: ['occassion_name'],
+    where: {
+      product_id : {
+        [Op.in]: product_list
+      }
+    }
   })
   
 
   var master_material = await models.product_materials.findAll({
     attributes: ['material_name'],
-    group: ['material_name']
-    // where: {
-    //   product_sku : {
-    //     [Op.in]: product_list
-    //   }
-    // }
+    group: ['material_name'],
+    where: {
+      product_sku : {
+        [Op.in]: product_list
+      }
+    }
   })
   var master_collection = await models.product_collections.findAll({
     attributes: ['collection_name'],
-    group: ['collection_name']
-    // where: {
-    //   product_id : {
-    //     [Op.in]: product_list
-    //   }
-    // }
+    group: ['collection_name'],
+    where: {
+      product_id : {
+        [Op.in]: product_list
+      }
+    }
   })
 
 
