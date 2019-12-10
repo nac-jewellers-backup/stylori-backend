fa46dbd1bc240f2a86b883dd00fd303bb70c84b6
Manokaran T <manokarantk@gmail.com>
2019-12-10 12:37:43 +0530

    enable cors policy

--- app.js
+++ app.js
@@ -12,11 +12,18 @@ const PgOrderByRelatedPlugin = require("@graphile-contrib/pg-order-by-related");
 const app = express();
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+ app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
 // const config = {
 //     user: 'mac',
 //     password: '12345',
