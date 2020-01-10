1a3a1d2608c9191159d9eba115d0f8e98fdf91b9
Crayond <crayond@Crayonds-iMac.local>
2020-01-10 17:09:29 +0530

    user registration

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -113,9 +113,9 @@ exports.signup = (req, res) => {
             var token = jwt.sign({ id: user.email }, process.env.SECRET, {
               expiresIn: '1d' // expires in 24 hours
             });
-            var emilreceipiants = [{to : email,subject:"You have successfully registered!"},{to : "dineshtawker@gmail.com"}]
+            var emilreceipiants = [{to : email,subject:"You have successfully registered!"}]
          
-            sendMail(emilreceipiants,emailTemp.getName(username))
+            sendMail(emilreceipiants,emailTemp.getName(firstname))
 
           res.send(200,{accessToken: token,user,user_profile_id: user_profile.id});
 
