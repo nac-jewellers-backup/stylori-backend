e077c0571a4409ee2274b785b4f89935d34341f7
Crayond <crayond@Crayonds-iMac.local>
2020-01-14 17:14:09 +0530

    update otp for guest login

--- controller/notify/Emailtemplate.js
+++ controller/notify/Emailtemplate.js
@@ -1,6 +1,10 @@
 const moment = require('moment');
 
 const getName = (username) => {
+    if(!username)
+    {
+      username = "User"
+    }
     return `<html>
       <head>
         <title>Registration Success</title>
@@ -27,7 +31,7 @@ const getName = (username) => {
             >
               <li style="display: inline; padding: 0px 3px; text-decoration: none;">
                 <a
-                  href=${process.env.baserl+"/pendants-jewellery"}
+                  href=${process.env.baseurl+"/pendants-jewellery"}
                   target="_blank"
                   style="text-decoration:none;color:#4F4C4C;"
                   >Pendants</a
@@ -36,7 +40,7 @@ const getName = (username) => {
               <li style="display: inline; padding: 0px 2px;">|</li>
               <li style="display: inline; padding: 0px 2px;">
                 <a
-                  href=${process.env.baserl+"/earrings-jewellery"}
+                  href=${process.env.baseurl+"/earrings-jewellery"}
                   target="_blank"
                   style="text-decoration:none;color:#4F4C4C;"
                   >Earrings</a
@@ -45,7 +49,7 @@ const getName = (username) => {
               <li style="display: inline; padding: 0px 2px;">|</li>
               <li style="display: inline; padding: 0px 2px;">
                 <a
-                  href=${process.env.baserl+"/bangles-jewellery"}
+                  href=${process.env.baseurl+"/bangles-jewellery"}
                   target="_blank"
                   style="text-decoration:none;color:#4F4C4C;"
                   >Bangles</a
@@ -54,7 +58,7 @@ const getName = (username) => {
               <li style="display: inline; padding: 0px 2px;">|</li>
               <li style="display: inline; padding: 0px 2px;">
                 <a
-                  href=${process.env.baserl+"/bangles-jewellery"}
+                  href=${process.env.baseurl+"/bangles-jewellery"}
                   target="_blank"
                   style="text-decoration:none;color:#4F4C4C;"
                   >Bracelets</a
@@ -63,7 +67,7 @@ const getName = (username) => {
               <li style="display: inline; padding: 0px 2px;">|</li>
               <li style="display: inline; padding: 0px 2px;">
                 <a
-                  href=${process.env.baserl+"/rings-jewellery"}
+                  href=${process.env.baseurl+"/rings-jewellery"}
                   target="_blank"
                   style="text-decoration:none;color:#4F4C4C;"
                   >Rings</a
@@ -72,7 +76,7 @@ const getName = (username) => {
               <li style="display: inline; padding: 0px 2px;">|</li>
               <li style="display: inline; padding: 0px 2px;">
                 <a
-                  href=${process.env.baserl+"/nose+pin+online-jewellery"}
+                  href=${process.env.baseurl+"/nose+pin+online-jewellery"}
                   target="_blank"
                   style="text-decoration:none;color:#4F4C4C;"
                   >Nose Pins</a
@@ -231,6 +235,10 @@ const getName = (username) => {
   };
   
   const forgotpasswordTemp = (username, email, token) => {
+    if(!username)
+      {
+        username = "User"
+      }
     return `<!doctype html>
       <html>
       <head>
@@ -292,6 +300,10 @@ const getName = (username) => {
   
   
   const changepasswordTemp = (username, email, token) => {
+      if(!username)
+      {
+        username = "User"
+      }
     return `<!doctype html>
       <html>
       <head>
@@ -313,25 +325,25 @@ const getName = (username) => {
               </tr>
               <tr>
                   <p style="padding:0px 15px;font-family: 'Arial', sans-serif;font-size: 12px;font-weight: 400;color: rgba(88,89,91,1.00); line-height:1.6;float:left;">
-                  Your password has been successfully changed.
+                  Greetings from Stylori.com.Your password has been changed successfully. In case you haven't requested for the change of password , please click here to reset your password
                   </p>
               </tr>
               <br/>
               <br/>
-              <tr>
-                  <p style="padding:0px 15px;font-family: 'Arial', sans-serif;font-size: 12px;font-weight: 400;color: rgba(88,89,91,1.00);line-height:1.6;">
-                  In case you haven't requested for the change of password , please click here to reset your password</p>
-              </tr>
+            //   <tr>
+            //       <p style="padding:0px 15px;font-family: 'Arial', sans-serif;font-size: 12px;font-weight: 400;color: rgba(88,89,91,1.00);line-height:1.6;">
+            //       In case you haven't requested for the change of password , please click here to reset your password</p>
+            //   </tr>
               <tr>
                   <p style="padding:0px 15px;font-family: 'Arial', sans-serif;font-size: 12px;font-weight: 400;color: #ed1164;">
-                      <span style="color: #ed1164;text-decoration:none;">${process.env.baseurl}</span>
+                      <span style="color: #ed1164;text-decoration:none;">${process.env.baseurl+'/forgotPassword'}</span>
                   </p>
               </tr>
+              
               <tr>
-                  <p style="padding:0px 15px;font-family: 'Arial', sans-serif;font-size: 12px;font-weight: 400;color: rgba(88,89,91,1.00);line-height:1.6;">If you did not request to have your password reset you can safely ignore this email.</p>
-              </tr>
-              <tr>
-                  <p style="padding:0px 15px;font-family: 'Arial', sans-serif;font-size: 12px;font-weight: 400;color: rgba(88,89,91,1.00);line-height:1.6;">If clicking the link does not seem to work, you can copy and paste the link into your browser's address window, or retype it there. Once you have returned to Stylori, we will give instructions for resetting your password.</p>
+                  <p style="padding:0px 15px;font-family: 'Arial', sans-serif;font-size: 12px;font-weight: 400;color: rgba(88,89,91,1.00);line-height:1.6;">
+                  If clicking the link does not seem to work, you can copy and paste the link into your browser's address window, or retype it there. Once you have returned to Stylori, we will give instructions for resetting your password.
+Stylori will never e-mail you and ask you to disclose or verify your Stylori password, credit card, or banking account number. If you receive a suspicious e-mail with a link to update your account information, do not click on the link--instead, report the e-mail to Stylori for investigation.</p>
               </tr>
               <tr>
                   <p style="padding:0px 15px;font-family: 'Arial', sans-serif;font-size: 11px;font-weight: 400;color: rgba(88,89,91,1.00);line-height:1.6;float: left;margin-bottom: 20px;">Stylori  will never e-mail you and ask you to disclose or verify your Stylori password, credit card, or banking account number. If you receive a suspicious e-mail with a link to update your account information, do not click on the link--instead, report the e-mail to Stylori for investigation. Greetings from Stylori.com</p>
