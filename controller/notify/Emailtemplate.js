3fe576510082b1d4ac7fab1b5acfefb1e7925004
Crayond <crayond@Crayonds-iMac.local>
2020-01-14 16:17:38 +0530

    change undefined in email template

--- controller/notify/Emailtemplate.js
+++ controller/notify/Emailtemplate.js
@@ -324,7 +324,7 @@ const getName = (username) => {
               </tr>
               <tr>
                   <p style="padding:0px 15px;font-family: 'Arial', sans-serif;font-size: 12px;font-weight: 400;color: #ed1164;">
-                      <span style="color: #ed1164;text-decoration:none;">${token}</span>
+                      <span style="color: #ed1164;text-decoration:none;">${process.env.baserl}</span>
                   </p>
               </tr>
               <tr>
@@ -423,136 +423,118 @@ const getName = (username) => {
     <html>
     <head>
     <meta charset="utf-8"/>
-    <link rel="stylesheet"  href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
     <title>Order Confirmation</title>
     </head>
-    
     <body>
-    
     <div style="width:600px;float:left;background:#FFFFFF;">
         <table style="width:600px;border-collapse: collapse;">
             <thead>
                 <!-- <img src="http://www.stylori.com/img/emailTemplate/hedear-bg.jpg" /> -->
-                
                 <img src="https://styloriimages.s3.ap-south-1.amazonaws.com/images/templates/cash-on-deli-hdr.jpg" />
             </thead>
             <tbody>
-    
                 <tr style="width:100%;">
-                <p style="padding:0px 15px; font-family: 'Arial', sans-serif;font-size: 12px;font-weight: 400;color: rgba(88,89,91,1.00); text-align: right;" >
-                    Order Placed On: 
+                <p style="padding:0px 15px; font-family: ‘Arial’, sans-serif;font-size: 12px;font-weight: 400;color: rgba(88,89,91,1.00); text-align: right;" >
+                    Order Placed On:
                      <span>${day}</span>
                 </p>
             </tr>
-            
              <tr style="width:100%;">
-                <p style="padding:0px 15px; font-family: 'Arial', sans-serif;font-size: 12px;font-weight: 400;color: rgba(88,89,91,1.00); text-align: right;" >
-                    Payment method: 
+                <p style="padding:0px 15px; font-family: ‘Arial’, sans-serif;font-size: 12px;font-weight: 400;color: rgba(88,89,91,1.00); text-align: right;" >
+                    Payment method:
                     <span >${paymentcontent.payment_mode}</span>
                 </p>
             </tr>
-            
             <tr style="width:100%;">
-                <p style="padding:0px 15px; font-family: 'Arial', sans-serif; font-size: 12px; font-weight: 500; color: rgba(237,18,95,1.00); text-align: right;">Order Number: <span >${paymentcontent.id}</span></p>
+                <p style="padding:0px 15px; font-family: ‘Arial’, sans-serif; font-size: 12px; font-weight: 500; color: rgba(237,18,95,1.00); text-align: right;">Order Number: <span >${paymentcontent.id}</span></p>
             </tr>
-            
              <tr style="width:100%;" >
-                <p style="padding:0px 15px; font-family: 'Arial', sans-serif; font-size: 12px; font-weight: 500; color: rgba(88,89,91,1.00);">
+                <p style="padding:0px 15px; font-family: ‘Arial’, sans-serif; font-size: 12px; font-weight: 500; color: rgba(88,89,91,1.00);">
                 <h5>Hello <span > ${paymentcontent.user_profile.first_name}</span>
                 <span ></span>
                 </h5>
                 </p>
             </tr>
-            
           <!--  <tr style="width:100%;">
-                <p style="padding:0px 15px; font-family: 'Arial', sans-serif; font-size: 12px; font-weight: 500; color: rgba(88,89,91,1.00);">
+                <p style="padding:0px 15px; font-family: ‘Arial’, sans-serif; font-size: 12px; font-weight: 500; color: rgba(88,89,91,1.00);">
                 <h5>Hello <span ></span></h5>
                 </p>
             </tr> -->
-            
              <tr style="width:100%;">
-                <p style="padding:0px 15px; font-family: 'Arial', sans-serif; font-size: 12px; font-weight: 400; color: rgba(88,89,91,1.00); line-height:1.6;">Thank You for placing your order with STYLORI, 
+                <p style="padding:0px 15px; font-family: ‘Arial’, sans-serif; font-size: 12px; font-weight: 400; color: rgba(88,89,91,1.00); line-height:1.6;">Thank You for placing your order with STYLORI,
                     Your order has been confirmed and is being processed. Here is the summary :</p>
             </tr>
-          
             <tr>
                 <table style="width:600px;padding: 0px 15px;text-align: left;border:none;border-collapse: collapse;margin-top: 15px;">
                     <thead>
-                    
-                     <tr style="border-top:1px solid #acacac;border-bottom:1px solid #acacac;">
-                   
-                        <th><p style="font-family: 'Arial', sans-serif;font-size: 12px;font-weight: bold;color: rgba(86,86,86,1.00);margin:0;padding: 10px 15px;">Item</p></th>
-                        <th><p style="font-family: 'Arial', sans-serif;font-size: 12px;font-weight: bold;color: rgba(86,86,86,1.00);margin:0;padding: 10px 0px;">Product Details</p></th>
-                        <th><p style="font-family: 'Arial', sans-serif;font-size: 12px;font-weight: bold;color: rgba(86,86,86,1.00);margin:0;padding: 10px 0px;text-align:right;">Quantity</p></th>
-                        <th><p style="font-family: 'Arial', sans-serif;font-size: 12px;font-weight: bold;color: rgba(86,86,86,1.00);margin:0;padding: 10px 5px 10px 0;text-align:right;">Sub Total</p></th>
+                     <tr style="border-top:1px solid #ACACAC;border-bottom:1px solid #ACACAC;">
+                        <th><p style="font-family: ‘Arial’, sans-serif;font-size: 12px;font-weight: bold;color: rgba(86,86,86,1.00);margin:0;padding: 10px 15px;">Item</p></th>
+                        <th><p style="font-family: ‘Arial’, sans-serif;font-size: 12px;font-weight: bold;color: rgba(86,86,86,1.00);margin:0;padding: 10px 0px;">Product Details</p></th>
+                        <th><p style="font-family: ‘Arial’, sans-serif;font-size: 12px;font-weight: bold;color: rgba(86,86,86,1.00);margin:0;padding: 10px 0px;text-align:right;">Quantity</p></th>
+                        <th><p style="font-family: ‘Arial’, sans-serif;font-size: 12px;font-weight: bold;color: rgba(86,86,86,1.00);margin:0;padding: 10px 5px 10px 0;text-align:right;">Sub Total</p></th>
                     </tr>
                     </thead>
                     <tbody>
                         ${prod_content}
-                        <tr style="border-top:1px solid #acacac;">
+                        <tr style="border-top:1px solid #ACACAC;">
                             <td></td>
                             <td></td>
-                            <td><p style="font-family: 'Arial', sans-serif;font-size: 12px;font-weight: 400;color: rgba(86,86,86,1.00);margin:0;text-align:right;margin-top:15px;padding-bottom:10px;">
+                            <td><p style="font-family: ‘Arial’, sans-serif;font-size: 12px;font-weight: 400;color: rgba(86,86,86,1.00);margin:0;text-align:right;margin-top:15px;padding-bottom:10px;">
                                    Total :  ${grossamt}<br />
                                 Shipping Charges:<br />
                                 Discount: ${discount}
                                 </p>
                             </td>
-                            <td><p style="font-family: 'Arial', sans-serif;font-size: 12px;font-weight: 400;color: rgba(86,86,86,1.00);margin:0;text-align:right;margin-top:15px;padding-bottom:10px;width: 100px;">
+                            <td><p style="font-family: ‘Arial’, sans-serif;font-size: 12px;font-weight: 400;color: rgba(86,86,86,1.00);margin:0;text-align:right;margin-top:15px;padding-bottom:10px;width: 100px;">
                                 <!-- <i class="fa fa-inr" aria-hidden="true"></i> --><img src="https://styloriimages.s3.ap-south-1.amazonaws.com/images/templates/rupee.png"/><span ></span>/- <br />
                                 Free<br />
                                <!-- <i class="fa fa-inr" aria-hidden="true"></i> --><img src="https://styloriimages.s3.ap-south-1.amazonaws.com/images/templates/rupee.png"/><span >0</span>/-
                                </p>
                             </td>
                         </tr>
-                        <tr style="border-top:1px solid #acacac;border-bottom:1px solid #acacac;">
-                            <td style="vertical-align: top;"><p style="font-family: 'Arial', sans-serif;font-size: 12px;font-weight: 400;color: rgba(86,86,86,1.00);margin:0;padding:5px 0 5px 15px;">* Inclusive of all taxes.</p></td>
+                        <tr style=border-top:1px solid #ACACAC;border-bottom:1px solid #ACACAC;">
+                            <td style="vertical-align: top;"><p style="font-family: ‘Arial’, sans-serif;font-size: 12px;font-weight: 400;color: rgba(86,86,86,1.00);margin:0;padding:5px 0 5px 15px;">* Inclusive of all taxes.</p></td>
                             <td></td>
-                            <td style="vertical-align: top;"><p style="font-family: 'Arial', sans-serif;font-size: 14px;color: rgba(207,32,72,1.00);
+                            <td style="vertical-align: top;"><p style="font-family: ‘Arial’, sans-serif;font-size: 14px;color: rgba(207,32,72,1.00);
     font-weight: bold;text-align:right;margin:0;padding:5px 0px;">Grand Total : ${discounted_price}</p></td>
-                            <td style="vertical-align: top;"><p style="font-family: 'Arial', sans-serif;font-size: 14px;color: rgba(207,32,72,1.00);
+                            <td style="vertical-align: top;"><p style="font-family: ‘Arial’, sans-serif;font-size: 14px;color: rgba(207,32,72,1.00);
     font-weight: bold;text-align:right;margin:0;padding:5px 0px;"><!-- <i class="fa fa-inr" aria-hidden="true"></i> --><img src="https://styloriimages.s3.ap-south-1.amazonaws.com/images/templates/rupee.png"/><span ></span>/-</p>
     </td>
                         </tr>
-                             
                     </tbody>
-                
                 </table>
             </tr>
               <tr>
                 <td>
-                 <p style="font-family: 'Arial', sans-serif; font-size: 12px; font-weight: 400; color: rgba(88,89,91,1.00); width:40%; padding-left: 15px;line-height:1.6;margin: 0;margin-top:15px;">
+                 <p style="font-family: ‘Arial’, sans-serif; font-size: 13px; font-weight: 600; color: rgba(88,89,91,1.00); width:40%; padding-left: 15px;line-height:1.6;margin: 0;margin-top:15px;">
                     Shipping Address:<br/>
                         <p ></p>
-                        <p>No 17/A Soundarya Nagar
+                        <p style="font-size: 13px; font-weight: 400;padding-left: 15px;color: #767D89;">No 17/A Soundarya Nagar
                             Narayanswamy Gardens, Kodungaiyur,</p>
-                        <p>Near Parvathy
+                        <p style="font-size: 13px; font-weight: 400;padding-left: 15px;color: #767D89;">Near Parvathy
                             Nagar Bus stand, Chennai, Tamil Nadu,</p>
-                        <p>India - 600118.</p>
+                        <p style="font-size: 13px; font-weight: 400;padding-left: 15px;color: #767D89;">India - 600118.</p>
                         </p>
                 </td>
             </tr>
-            
-            
             <tr>
-                            <p style="padding:0px 15px;line-height:1.5; font-family: 'Arial', sans-serif;font-size: 12px;font-weight: 400;color: rgba(86,86,86,1.00);margin:0;padding-top: 15px;"> You can visit <a href="/account/orders">https://www.stylori.com/account/orders</a> to view your order status and to contact us regarding this order.<br />
+                            <p style="padding:0px 15px;line-height:1.5; font-family: ‘Arial’, sans-serif;font-size: 12px;font-weight: 400;color: rgba(86,86,86,1.00);margin:0;padding-top: 15px;"> You can visit <a href="/account/orders">https://www.stylori.com/account-allorders</a> to view your order status and to contact us regarding this order.<br />
             Should you find the details of the order incorrect, please feel free to call us at 18001020330 or email us at hello@stylori.com </p>
-          <p style="line-height:1.5;padding:0px 15px;font-family: 'Arial', sans-serif;font-size: 12px;font-weight: 400;color: rgba(86,86,86,1.00);margin:15px 0;">Team Stylori <br />
+          <p style="line-height:1.5;padding:0px 15px;font-family: ‘Arial’, sans-serif;font-size: 12px;font-weight: 400;color: rgba(86,86,86,1.00);margin:15px 0;">Team Stylori <br />
               With Love </p>
-                        
                         </tr>
             </tbody>
         </table>
-        
-         <div style="position: relative;
-    min-height: 1px;
-    padding-right: 15px;
-    padding-left: 15px;
+         <div class="container" style="position: relative;
+       height: 35px;
+    align-items: center;
+    display: flex;
     width: 100%;
-    background: rgba(215,221,249,1.00);
-    padding: 10px 0px 20px 0px;">
+    background: rgba(215,221,249,1.00);"
+>
                 <p style="padding: 0px 15px;
-    font-family: 'Arial', sans-serif;
+    font-family: ‘Arial’, sans-serif;
     font-size: 12px;
     font-weight: 400;
     color: rgba(88,89,91,1.00);
@@ -560,7 +542,7 @@ const getName = (username) => {
     float: left;
     margin: 0;">Customer Care: 18001020330</p>
                 <p style="padding: 0px 15px;
-    font-family: 'Arial', sans-serif;
+    font-family: ‘Arial’, sans-serif;
     font-size: 12px;
     font-weight: 400;
     color: rgba(88,89,91,1.00);
@@ -571,8 +553,6 @@ const getName = (username) => {
     position: relative;
     left: 130px;">Contact: hello@stylori.com</p>
             </div>
-        
-        
     </div>
     </body>
     </html>
