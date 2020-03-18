849c572570a81f19a00380f00deaf11d6f5ad88a
Crayond <crayond@Crayonds-iMac.local>
2020-03-18 17:06:56 +0530

    update hsn number for product

--- migrations/20190919113059-create-master-tax-settings.js
+++ migrations/20190919113059-create-master-tax-settings.js
@@ -7,7 +7,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.UUID
       },
-      product_type: {
+      hsn_number: {
         type: Sequelize.STRING
       },
       tax_name: {
