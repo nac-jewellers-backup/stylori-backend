fa7df1ef688a39ebb68afbe56358e8c25d024256
Mano <manokarantk@gmail.com>
2020-04-24 11:32:48 +0530

    price update

--- controller/pricingcontroller_whole.js
+++ controller/pricingcontroller_whole.js
@@ -47,9 +47,9 @@ exports.priceupdate = (req, res) => {
     const {req_product_id,generatedSku,history_id, vendorcode,category,product_type,metalpurity,product_category,pricingcomponent,purity,sizes,diamondtypes} = req.body
     var whereclause1 = {
       isactive : true,
-      // product_id: {
-      //   [Op.iLike]:'%SR0185%'
-      // }
+      product_id: {
+        [Op.iLike]:'%SR0010%'
+      }
     }
     price_running_id = history_id
     console.log(":>>>>>>>>>1212")
@@ -837,7 +837,7 @@ exports.priceupdate = (req, res) => {
       console.log("processlength"+product_obj.trans_sku_lists.length)
       if(pricingcomponent)
       {
-      checkisinclude();
+       checkisinclude();
 
       }else{
         updatediamondprice(productobj.vendor_code, productskus[0])
@@ -907,8 +907,7 @@ exports.priceupdate = (req, res) => {
 
        function diamond_process(diamondobj,vendorcode)
        {
-         console.log(">>>>>>>>")
-         console.log(processcount)
+       
 
 
           var conditionobj = {
@@ -959,7 +958,6 @@ exports.priceupdate = (req, res) => {
                     models.pricing_sku_materials.findOne({
                       where: {product_id: product_obj.product_id, material_name :materialname }
                     }).then(price_splitup_model=> {
-                      console.log(JSON.stringify(price_splitup_model))
 
                       if(price_splitup_model)
                       {
@@ -1027,8 +1025,8 @@ exports.priceupdate = (req, res) => {
 
                   if(product_diamonds.length > processcount)
                   {
-                    console.log("diamondname")
-                    console.log(JSON.stringify(product_diamonds[processcount]))
+                    // console.log("diamondname")
+                    // console.log(JSON.stringify(product_diamonds[processcount]))
 
                   diamond_process(product_diamonds[processcount],vendorcode)
                   }else{
@@ -1693,8 +1691,6 @@ exports.priceupdate = (req, res) => {
 
           let sku_margin = ((total_sellingprice - total_costprice)/total_costprice)*100
           let markupobj =  await materialmarkupval(total_sellingprice,product_obj.product_category)
-          console.log("manomarkuptest")
-          console.log(JSON.stringify(markupobj))
           var goldmarkupvalue = goldsellingprice;
           let skus_arrr = [];
           var discounttitle = ""
@@ -1722,11 +1718,7 @@ exports.priceupdate = (req, res) => {
 
                                          
             })
-            console.log("CKDHDJDJJDJD")
-            console.log(JSON.stringify(discounttitle))
-
-           console.log(JSON.stringify(disscount_obj))
-           console.log(JSON.stringify(skus_arrr))
+      
 
           var mkcharge_discount  = 0;
           var diamond_discount  = 0;
@@ -1778,11 +1770,7 @@ exports.priceupdate = (req, res) => {
 
             }
            } 
-           console.log("CKDHDJDJJDJD")
-           console.log(mkcharge_discount)
-           console.log(diamond_discount)
-           console.log(gemstone_discount)
-           console.log(metal_discount)
+
 
           var makingchargemarkupvalue = makingsellingprice;
           var makingchargediscountvalue = ((makingchargemarkupvalue * 100) /(100 - mkcharge_discount));
@@ -1796,8 +1784,7 @@ exports.priceupdate = (req, res) => {
 
           
           markupobj.forEach(async markup => {
-                console.log("XXXXXXXXXXXXXXX")
-                console.log(markup.material)
+           
                 if(markup.material == 'Gold')
                   {
                     goldmarkupvalue = (goldsellingprice + (goldsellingprice * (markup.markup_value/100)))
@@ -1831,17 +1818,13 @@ exports.priceupdate = (req, res) => {
                   if(markup.material == 'Diamond')
                   {
                     diamondmarkupvalue = (diamondsellingprice + (diamondsellingprice * (markup.markup_value/100)))
-                    console.log("diamondmarkup_mano")
-                    console.log(diamondmarkupvalue)
+                    console.log("log diamond")
+                    
                     diamonddiscountvalue = ((diamondmarkupvalue * 100) /(100 - diamond_discount));
 
                     var query = "UPDATE pricing_sku_materials SET markup = (selling_price + (selling_price *"+markup.markup_value+"/100)) where product_sku ='"+productskus[skucount].generated_sku+"' and component LIKE 'diamond%'" ;
-                    console.log(query)
 
                     await models.sequelize.query(query).then(([results, metadata]) => {
-                      console.log(JSON.stringify(metadata))
-                      console.log(JSON.stringify(results))
-
                       // Results will be an empty array and metadata will contain the number of affected rows.
                     })
                   }
@@ -1890,8 +1873,7 @@ exports.priceupdate = (req, res) => {
          }
          
          total_sku_discountvalue = makingchargediscountvalue + golddiscountvalue + gemstonediscountvalue + diamonddiscountvalue;
-         console.log("testing error")
-        console.log(discount_price_distribute_percentage)
+       
         
         var mkquery = "UPDATE pricing_sku_metals SET discount_price = ((markup * 100) /(100 - "+mkcharge_discount+") + ("+golddiscount_different+" * ("+discount_price_distribute_percentage+" + "+makingcharge_percentage+" ))) where product_sku ='"+productskus[skucount].generated_sku+"' and material_name = 'makingcharge'" ;
           await models.sequelize.query(mkquery).then(([results, metadata]) => {
@@ -1994,8 +1976,7 @@ exports.priceupdate = (req, res) => {
               // let taxobj = {
               //   tax_value : 3
               // }
-              console.log("taxcomponent")
-              console.log(JSON.stringify(product_obj.hsn_number))
+    
               let taxval = 3;
               if(taxobj)
               {
