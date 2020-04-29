29fd870d1ab53d836e7bb2d610127742caea8fe0
Mano <manokarantk@gmail.com>
2020-04-29 11:21:16 +0530

    update route

--- routes/route.js
+++ routes/route.js
@@ -155,7 +155,7 @@ module.exports = function(app) {
 	app.post('/updateskuinfo', productcontroller.updateskuinfo);
 	app.post('/updateskupriceinfo', productcontroller.updateskupriceinfo);
 	app.post('/editproductgemstone', productcontroller.editproductgemstone);
-	app.post('/updateproductattr', productcontroller.updateproductattr);
+	app.post('/updateproductattr', productcontroller.updateproductattr_bk);
 	app.post('/updateproductimage', productcontroller.updateproductimage);
 
 	
