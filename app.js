dd49f90003b0a7c480c0259e0012af0409d211c3
manokarantk <manokarantk@gmail.com>
2021-02-01 13:07:44 +0530

    update cors

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(function (req, res, next) {
   }
   //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
   res.header('Access-Control-Allow-Methods', '*');
-  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Headers', '*');  
   res.header('Access-Control-Allow-Credentials', true);
   return next();
   });
