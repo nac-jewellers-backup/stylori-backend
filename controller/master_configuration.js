455b0434e711a871251b5c156e01b5ff582575d4
manokarantk <manokarantk@gmail.com>
2021-02-08 11:29:59 +0530

    update manage category api

--- controller/master_configuration.js
+++ controller/master_configuration.js
@@ -163,7 +163,7 @@ exports.managecategories = async (req, res) => {
             short_code : shortCode,
             is_filter: isFilter,
             is_active: isActive,
-            alias : 'CAT'+pad((categoryobj.alias_id  + 1),3),
+            alias : 'CAT'+pad(( parseInt(categoryobj.alias_id)+ 1),3),
             filter_order : filterOrder
             }
         await models.master_product_categories.create(   
