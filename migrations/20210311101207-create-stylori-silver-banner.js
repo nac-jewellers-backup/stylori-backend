8145fab6cff512c1b339dcce01d1444afcf079cd
vardhman <vardhman@crayond.co>
2021-04-23 16:22:26 +0530

    added top_carousel

--- migrations/20210311101207-create-stylori-silver-banner.js
+++ migrations/20210311101207-create-stylori-silver-banner.js
@@ -1,36 +1,39 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('stylori_silver_banners', {
+    return queryInterface.createTable("stylori_silver_banners", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       url: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
       },
       mobile: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
       },
       web: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
       },
       position: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+      },
+      url_param: {
+        type: Sequelize.TEXT,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('stylori_silver_banners');
-  }
+    return queryInterface.dropTable("stylori_silver_banners");
+  },
 };
--- models/stylori_banner.js
+++ models/stylori_banner.js
@@ -1,12 +1,17 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const stylori_banner = sequelize.define('stylori_banner', {
-    url: DataTypes.STRING,
-    mobile: DataTypes.STRING,
-    web: DataTypes.STRING,
-    position: DataTypes.INTEGER
-  }, {});
-  stylori_banner.associate = function(models) {
+  const stylori_banner = sequelize.define(
+    "stylori_banner",
+    {
+      url: DataTypes.STRING,
+      mobile: DataTypes.STRING,
+      web: DataTypes.STRING,
+      position: DataTypes.INTEGER,
+      url_param: DataTypes.STRING,
+    },
+    {}
+  );
+  stylori_banner.associate = function (models) {
     // associations can be defined here
   };
   return stylori_banner;
